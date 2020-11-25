2fa7ea864514232e8c8bc31dd8b2a1513400ece6
Cameron Katri <me@cameronkatri.com>
2020-11-25 13:50:35 -0500

    Add options to not generate cy+ and gsc packages

--- src/main.m
+++ src/main.m
@@ -1,8 +1,49 @@
 #import "Firmware.h"
 
+#include <getopt.h>
+
 #define FIRMWARE_VERSION 6
 
-int main() {
+int main(int argc, char *argv[]) {
+
+    int nocy = 0;
+    int nogsc = 0;
+    int showhelp = 0;
+
+    struct option longOptions[] = {
+        { "nocy", no_argument, 0, 'c' },
+        { "nogsc", no_argument, 0, 'g' },
+        { "help", no_argument, 0, 'h' },
+        { NULL, 0, NULL, 0 }
+    };
+
+    int index = 0, opt = 0;
+
+    while ((opt = getopt_long(argc, argv, "cgh", longOptions, &index)) != -1) {
+        switch (opt) {
+            case 'c':
+                nocy = 1;
+                break;
+            case 'g':
+                nogsc = 1;
+                break;
+            case 'h':
+                showhelp = 1;
+                break;
+         }
+    }
+
+    if (showhelp) {
+        printf(
+            "Usage: %s [OPTION]\n\n"
+            "  -c, --nocy   Do not generate the packages starting with cy+\n"
+            "  -g, --nogsc  Do not generate the capability packages\n"
+            "  -h, --help   Give this help list.\n",
+            argv[0]
+        );
+        return 0;
+    }
+
     NSLog(@"[Firmware] full steam ahead");
 
     Firmware *firmware = [[Firmware alloc] init];
@@ -12,7 +53,8 @@ int main() {
 
     // generate device specific packages
 
-    [firmware generateCapabilityPackages];
+    if (!nogsc)
+        [firmware generateCapabilityPackages];
 
 
     // generate always needed packages
@@ -23,19 +65,21 @@ int main() {
         [firmware generatePackage:@"firmware" forVersion:iosVersion withDescription:@"almost impressive Apple frameworks" andName:@"iOS Firmware"];
     }
 
-    NSString *packageName = [@"cy+os." stringByAppendingString:[device getOperatingSystem]];
-    [firmware generatePackage:packageName forVersion:iosVersion withDescription:@"virtual operating system dependency"];
+    if (!nocy) {
+        NSString *packageName = [@"cy+os." stringByAppendingString:[device getOperatingSystem]];
+        [firmware generatePackage:packageName forVersion:iosVersion withDescription:@"virtual operating system dependency"];
 
-    packageName = [@"cy+cpu." stringByAppendingString:device.cpuArchitecture];
-    [firmware generatePackage:packageName forVersion:@"0" withDescription:@"virtual CPU dependency"];
+        packageName = [@"cy+cpu." stringByAppendingString:device.cpuArchitecture];
+        [firmware generatePackage:packageName forVersion:@"0" withDescription:@"virtual CPU dependency"];
 
-    packageName = [@"cy+model." stringByAppendingString:[device getModelName]];
-    [firmware generatePackage:packageName forVersion:[device getModelVersion] withDescription:@"virtual model dependency"];
+        packageName = [@"cy+model." stringByAppendingString:[device getModelName]];
+        [firmware generatePackage:packageName forVersion:[device getModelVersion] withDescription:@"virtual model dependency"];
 
-    packageName = [@"cy+kernel." stringByAppendingString:[device getOperatingSystemType]];
-    [firmware generatePackage:packageName forVersion:[device getOperatingSystemRelease] withDescription:@"virtual kernel dependency"];
+        packageName = [@"cy+kernel." stringByAppendingString:[device getOperatingSystemType]];
+        [firmware generatePackage:packageName forVersion:[device getOperatingSystemRelease] withDescription:@"virtual kernel dependency"];
 
-    [firmware generatePackage:@"cy+lib.corefoundation" forVersion:[device getCoreFoundationVersion] withDescription:@"virtual corefoundation dependency"];
+        [firmware generatePackage:@"cy+lib.corefoundation" forVersion:[device getCoreFoundationVersion] withDescription:@"virtual corefoundation dependency"];
+    }
 
 
     [firmware writePackagesToStatusFile];
